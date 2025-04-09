fbf560d75b73b3ea0fcc4078bd3840d3b194d057
omnipro-bh <143381088+omnipro-bh@users.noreply.github.com>
2025-04-09 16:44:56 +0300

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -296,8 +296,8 @@
             welcomeText: '',
             responseTimeText: '',
             poweredBy: {
-                text: 'Powered by n8n',
-                link: 'https://n8n.partnerlinks.io/m8a94i19zhqq?utm_source=nocodecreative.io'
+                text: 'Powered by Omni Pro',
+                link: 'https://omnipro-bh.com/'
             }
         },
         style: {
